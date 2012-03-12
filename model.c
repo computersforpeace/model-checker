443c3b5f3962dbd4d5607a11d9c41ececf854e2c
Brian Norris <banorris@uci.edu>
2012-03-12 15:51:56 -0700

    model: add global model_checker initialization

--- model.c
+++ model.c
@@ -1,1 +1,23 @@
 #include "model.h"
+#include "schedule.h"
+#include <stdlib.h>
+#include <string.h>
+
+struct model_checker *model;
+
+void model_checker_init(void)
+{
+	model = malloc(sizeof(*model));
+	memset(model, 0, sizeof(*model));
+	scheduler_init(model);
+}
+
+void model_checker_exit(void)
+{
+	struct scheduler *sched = model->scheduler;
+
+	if (sched->exit)
+		sched->exit();
+	free(sched);
+	free(model);
+}
--- model.h
+++ model.h
@@ -5,4 +5,8 @@ struct model_checker {
 	struct scheduler *scheduler;
 };
 
+extern struct model_checker *model;
+
+void model_checker_init(void);
+
 #endif /* __MODEL_H__ */

0c8ec8cc34f3ed7ab5398c65d58129e604b73920
Brian Norris <banorris@uci.edu>
2012-04-10 15:19:29 -0700

    userprog: use both atomic loads and stores
    (Meaningless change to 'user' program; just for testing purposes)

--- userprog.c
+++ userprog.c
@@ -9,8 +9,14 @@ static void a(atomic_int *obj)
 
 	for (i = 0; i < 10; i++) {
 		printf("Thread %d, loop %d\n", thread_current()->id, i);
-		if (i % 2)
+		switch (i % 4) {
+		case 1:
 			atomic_load(obj);
+			break;
+		case 3:
+			atomic_store(obj, i);
+			break;
+		}
 	}
 }
 
@@ -19,11 +25,11 @@ void user_main()
 	struct thread t1, t2;
 	atomic_int obj;
 
-	printf("%s() creating 2 threads\n", __func__);
+	printf("Thread %d creating 2 threads\n", thread_current()->id);
 	thread_create(&t1, (void (*)())&a, &obj);
 	thread_create(&t2, (void (*)())&a, &obj);
 
 	thread_join(&t1);
 	thread_join(&t2);
-	printf("%s() is finished\n", __func__);
+	printf("Thread %d is finished\n", thread_current()->id);
 }

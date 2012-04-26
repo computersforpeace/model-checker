38de1b8a5d58d70bda97ad3b453b62d721f4f7ed
Brian Norris <banorris@uci.edu>
2012-04-26 10:19:12 -0700

    librace: format DEBUG() prints properly
    Apparently ISO C99 defines macros PRIuXX for proper printing of different-sized
    uints.

--- librace.cc
+++ librace.cc
@@ -1,24 +1,27 @@
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+
 #include "librace.h"
 #include "common.h"
 
 void store_8(void *addr, uint8_t val)
 {
-	DEBUG("addr = %p, val = %u\n", addr, val);
+	DEBUG("addr = %p, val = %" PRIu8 "\n", addr, val);
 }
 
 void store_16(void *addr, uint16_t val)
 {
-	DEBUG("addr = %p, val = %u\n", addr, val);
+	DEBUG("addr = %p, val = %" PRIu16 "\n", addr, val);
 }
 
 void store_32(void *addr, uint32_t val)
 {
-	DEBUG("addr = %p, val = %u\n", addr, val);
+	DEBUG("addr = %p, val = %" PRIu32 "\n", addr, val);
 }
 
 void store_64(void *addr, uint64_t val)
 {
-	DEBUG("addr = %p, val = %llu\n", addr, val);
+	DEBUG("addr = %p, val = %" PRIu64 "\n", addr, val);
 }
 
 uint8_t load_8(void *addr)

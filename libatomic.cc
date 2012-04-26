9ed7200f1ffce5963e5196aa82c2406a62a107d2
Brian Norris <banorris@uci.edu>
2012-04-26 11:47:22 -0700

    libatomic: add DBG() calls

--- libatomic.cc
+++ libatomic.cc
@@ -1,13 +1,16 @@
 #include "libatomic.h"
 #include "model.h"
+#include "common.h"
 
 void atomic_store_explicit(struct atomic_object *obj, int value, memory_order order)
 {
+	DBG();
 	model->switch_to_master(new ModelAction(ATOMIC_WRITE, order, obj, value));
 }
 
 int atomic_load_explicit(struct atomic_object *obj, memory_order order)
 {
+	DBG();
 	model->switch_to_master(new ModelAction(ATOMIC_READ, order, obj, VALUE_NONE));
 	return 0;
 }

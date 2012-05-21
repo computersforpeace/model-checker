c77d1d922ed975933eab0b527c2c2e2951b64174
Brian Norris <banorris@uci.edu>
2012-05-21 10:32:05 -0700

    run.sh: provide gdb option

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=.
-./model
+
+if [ $# -gt 0 ]; then
+	if [ "$1" = "gdb" ]; then
+		gdb ./model
+	else
+		echo "Invalid argument(s)"
+		exit 1
+	fi
+else
+	./model
+fi

99b073e58b66c9df66bbbc7e0396f757bde9e03a
Brian Norris <banorris@uci.edu>
2012-05-25 19:00:37 -0700

    action: use proper location comparison
    Now that snapshotting is in place for the initial state, stack allocation is
    deterministic across executions, so we can compare stack-allocated addresses
    again.

--- action.cc
+++ action.cc
@@ -52,8 +52,7 @@ bool ModelAction::is_release()
 
 bool ModelAction::same_var(ModelAction *act)
 {
-	return true;
-	// TODO: fix stack allocation... return location == act->location;
+	return location == act->location;
 }
 
 bool ModelAction::same_thread(ModelAction *act)

a59176e8094c3a7ec272f70b7f9511f9061dd117
Brian Demsky <bdemsky@uci.edu>
2012-07-06 00:39:00 -0700

    add forgetten file

---
+++ config.h
@@ -0,0 +1,11 @@
+/** @file config.h
+ * @brief Configuration file.
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+/** Do we have a 48 bit virtual address (64 bit machine) or 32 bit addresses */
+#define BIT48 1
+
+
+#endif

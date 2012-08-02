fb8f7f7b022e4a45fb1f918a43dba03f2336435a
Brian Norris <banorris@uci.edu>
2012-08-02 10:59:16 -0700

    hashtable: add some documentation

--- hashtable.h
+++ hashtable.h
@@ -8,6 +8,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * Hashtable linked node class, for chained storage of hash table conflicts. By
+ * default it is snapshotting, but you can pass in your own allocation
+ * functions.
+ *
+ * @tparam _Key    Type name for the key
+ * @tparam _Val    Type name for the values to be stored
+ * @tparam _malloc Provide your own 'malloc' for the table, or default to
+ *                 snapshotting.
+ * @tparam _calloc Provide your own 'calloc' for the table, or default to
+ *                 snapshotting.
+ * @tparam _free   Provide your own 'free' for the table, or default to
+ *                 snapshotting.
+ */
 template<typename _Key, typename _Val, void * (* _malloc)(size_t), void * (* _calloc)(size_t, size_t), void (*_free)(void *)>
 struct hashlistnode {
 	_Key key;
@@ -32,12 +46,31 @@ struct hashlistnode {
 };
 
 /**
- * Hashtable class.  By default it is snapshotting, but you can pass in your
- * own allocation functions.
+ * Hashtable class. By default it is snapshotting, but you can pass in your own
+ * allocation functions.
+ *
+ * @tparam _Key    Type name for the key
+ * @tparam _Val    Type name for the values to be stored
+ * @tparam _KeyInt Integer type that is at least as large as _Key. Used for key
+ *                 manipulation and storage.
+ * @tparam _Shift  Logical shift to apply to all keys. Default 0.
+ * @tparam _malloc Provide your own 'malloc' for the table, or default to
+ *                 snapshotting.
+ * @tparam _calloc Provide your own 'calloc' for the table, or default to
+ *                 snapshotting.
+ * @tparam _free   Provide your own 'free' for the table, or default to
+ *                 snapshotting.
  */
 template<typename _Key, typename _Val, typename _KeyInt, int _Shift=0, void * (* _malloc)(size_t)=malloc, void * (* _calloc)(size_t, size_t)=calloc, void (*_free)(void *)=free>
 	class HashTable {
  public:
+	/**
+	 * Constructor
+	 * @param initialcapacity Sets the initial capacity of the hash table.
+	 * Default size 1024.
+	 * @param factor Sets the percentage full before the hashtable is
+	 * resized. Default ratio 0.5.
+	 */
 	HashTable(unsigned int initialcapacity=1024, double factor=0.5) {
 		// Allocate space for the hash table
 		table = (struct hashlistnode<_Key,_Val, _malloc, _calloc,_free> **) _calloc(initialcapacity, sizeof(struct hashlistnode<_Key,_Val, _malloc, _calloc, _free> *));
@@ -48,6 +81,7 @@ template<typename _Key, typename _Val, typename _KeyInt, int _Shift=0, void * (*
 		size = 0; // Initial number of elements in the hash
 	}
 
+	/** Destructor */
 	~HashTable() {
 		for(unsigned int i=0;i<capacity;i++) {
 			struct hashlistnode<_Key,_Val, _malloc, _calloc, _free> * bin = table[i];
@@ -60,18 +94,22 @@ template<typename _Key, typename _Val, typename _KeyInt, int _Shift=0, void * (*
 		_free(table);
 	}
 
+	/** Override: new operator */
 	void * operator new(size_t size) {
 		return _malloc(size);
 	}
 
+	/** Override: delete operator */
 	void operator delete(void *p, size_t size) {
 		_free(p);
 	}
 
+	/** Override: new[] operator */
 	void * operator new[](size_t size) {
 		return _malloc(size);
 	}
 
+	/** Override: delete[] operator */
 	void operator delete[](void *p, size_t size) {
 		_free(p);
 	}

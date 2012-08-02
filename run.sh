66a8501ed6eecc5e1ce308235ad7226da363b70e
Brian Norris <banorris@uci.edu>
2012-08-02 16:37:03 -0700

    run.sh: fixup run script
    run.sh will, by default, run ./test/userprog.o now. This can be modified later
    to provide several regression tests in-tree, with scripted execution of the
    tests.

--- run.sh
+++ run.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
+#
+# Runs a simple test (default: ./test/userprog.o)
+# Syntax:
+#  ./run.sh [gdb]
+#  ./run.sh [test program] [gdb]
+#
+# If you include a 'gdb' argument, the your program will be launched with gdb.
+# You can also supply a test program argument to run something besides the
+# default program.
+#
+
+BIN=./test/userprog.o
+
 export LD_LIBRARY_PATH=.
+
+[ $# -gt 0 ] && [ "$1" != "gdb" ] && BIN=$1 && shift
 
 if [ $# -gt 0 ]; then
 	if [ "$1" = "gdb" ]; then
-		gdb ./model
+		gdb $BIN
 	else
 		echo "Invalid argument(s)"
 		exit 1
 	fi
 else
-	./model
+	$BIN
 fi

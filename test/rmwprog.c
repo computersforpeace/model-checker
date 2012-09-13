09592530fc40cdf3eb049c1adb7f26066a450e16
Brian Demsky <bdemsky@uci.edu>
2012-09-12 22:59:03 -0700

    commit new test case

---
+++ test/rmwprog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include "libthreads.h"
+#include "librace.h"
+#include "stdatomic.h"
+
+atomic_int x;
+
+static void a(void *obj)
+{
+	atomic_fetch_add_explicit(&x, 1, memory_order_relaxed);
+	atomic_fetch_add_explicit(&x, 1, memory_order_relaxed);
+}
+
+void user_main()
+{
+	thrd_t t1, t2;
+
+	atomic_init(&x, 0);
+
+	thrd_create(&t1, (thrd_start_t)&a, NULL);
+	thrd_create(&t2, (thrd_start_t)&a, NULL);
+
+	thrd_join(t1);
+	thrd_join(t2);
+}

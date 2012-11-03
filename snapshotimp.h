6f2d14687b0bc7063946d5dfca05cf47330eef4e
Brian Norris <banorris@uci.edu>
2012-11-03 14:55:56 -0700

    snapshot: read from /proc/self/maps
    We don't need to get the PID

--- snapshotimp.h
+++ snapshotimp.h
@@ -9,7 +9,6 @@
 #include <inttypes.h>
 #include <fcntl.h>
 #include <sys/mman.h>
-#include <sys/types.h>
 #include <csignal>
 #define SHARED_MEMORY_DEFAULT  (100 * ((size_t)1 << 20)) // 100mb for the shared memory
 #define STACK_SIZE_DEFAULT      (((size_t)1 << 20) * 20)  // 20 mb out of the above 100 mb for my stack

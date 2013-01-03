9fbf5dd29dd143dfbf4acef3d1ec11e0758fccb1
Brian Norris <banorris@uci.edu>
2013-01-02 17:21:33 -0800

    snapshotimp: move macros to fork-based
    These macros are only used for fork-based snapshotting.

--- snapshotimp.h
+++ snapshotimp.h
@@ -9,9 +9,6 @@
 
 #include "snapshot.h"
 
-#define SHARED_MEMORY_DEFAULT  (100 * ((size_t)1 << 20)) // 100mb for the shared memory
-#define STACK_SIZE_DEFAULT      (((size_t)1 << 20) * 20)  // 20 mb out of the above 100 mb for my stack
-
 #if USE_MPROTECT_SNAPSHOT
 //Each snapshotrecord lists the firstbackingpage that must be written to revert to that snapshot
 struct SnapShotRecord {
@@ -52,6 +49,9 @@ struct SnapShot {
 #else
 
 #include <ucontext.h>
+
+#define SHARED_MEMORY_DEFAULT  (100 * ((size_t)1 << 20)) // 100mb for the shared memory
+#define STACK_SIZE_DEFAULT      (((size_t)1 << 20) * 20)  // 20 mb out of the above 100 mb for my stack
 
 struct SnapShot {
 	void *mSharedMemoryBase;

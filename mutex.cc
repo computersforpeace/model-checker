11bd7a3e906795b46b1fd20276a28002978bf1ab
Brian Norris <banorris@uci.edu>
2013-03-02 22:55:51 -0800

    mutex: fix indentation

--- mutex.cc
+++ mutex.cc
@@ -6,23 +6,28 @@
 #include "action.h"
 
 namespace std {
-mutex::mutex() {
-	state.islocked=false;
-	thread_id_t tid=thread_current()->get_id();
-	state.alloc_tid=tid;
-	state.alloc_clock=model->get_cv(tid)->getClock(tid);
+
+mutex::mutex()
+{
+	state.islocked = false;
+	thread_id_t tid = thread_current()->get_id();
+	state.alloc_tid = tid;
+	state.alloc_clock = model->get_cv(tid)->getClock(tid);
 }
 	
-void mutex::lock() {
-  model->switch_to_master(new ModelAction(ATOMIC_LOCK, std::memory_order_seq_cst, this));
+void mutex::lock()
+{
+	model->switch_to_master(new ModelAction(ATOMIC_LOCK, std::memory_order_seq_cst, this));
 }
 	
-bool mutex::try_lock() {
-  return model->switch_to_master(new ModelAction(ATOMIC_TRYLOCK, std::memory_order_seq_cst, this));
+bool mutex::try_lock()
+{
+	return model->switch_to_master(new ModelAction(ATOMIC_TRYLOCK, std::memory_order_seq_cst, this));
 }
 
-void mutex::unlock() {
-  model->switch_to_master(new ModelAction(ATOMIC_UNLOCK, std::memory_order_seq_cst, this));
+void mutex::unlock()
+{
+	model->switch_to_master(new ModelAction(ATOMIC_UNLOCK, std::memory_order_seq_cst, this));
 }
 
 }

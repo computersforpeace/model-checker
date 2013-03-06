7fb8a4d038c69c14a3ac9cc8be16192712175004
Brian Norris <banorris@uci.edu>
2013-03-05 18:33:43 -0800

    impatomic: fences linker error
    Because these functions were declared in a header (but not static), they
    could get linked into multiple object files, causing linker errors. For
    now, just make them 'static inline'. I'm not sure if that exactly fits
    the spec, but it's close enough for now.

--- include/impatomic.h
+++ include/impatomic.h
@@ -3902,11 +3902,11 @@ T* atomic<T*>::fetch_sub( ptrdiff_t __v__, memory_order __x__ ) volatile
 #ifdef __cplusplus
 extern "C" {
 #endif
-inline void atomic_thread_fence(memory_order order)
+static inline void atomic_thread_fence(memory_order order)
 { _ATOMIC_FENCE_(order); }
 
 /** @todo Do we want to try to support a user's signal-handler? */
-inline void atomic_signal_fence(memory_order order)
+static inline void atomic_signal_fence(memory_order order)
 { /* No-op? */ }
 #ifdef __cplusplus
 }
